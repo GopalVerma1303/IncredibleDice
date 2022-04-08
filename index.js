375ec756b5bf9dc7eb1191e3dd2a95e0d123d147
Gopal Verma <67120469+GopalVerma1303@users.noreply.github.com>
2022-04-08 15:47:39 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+var randomNumber1 = Math.floor(Math.random()*6)+1;
+var randomDiceImage1 = "dice" + randomNumber1 + ".png";
+var randomDiceSource1 = "/images/" + randomDiceImage1;
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", randomDiceSource1);
+
+var randomNumber2 = Math.floor(Math.random()*6)+1;
+var randomDiceImage2 = "dice" + randomNumber2 + ".png";
+var randomDiceSource2 = "/images/" + randomDiceImage2;
+var image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src", randomDiceSource2);
+ 
+
+// var resultImage1 = "<img src = " + "/images" + "/i1.ico";
+var resultImage1 = "<img "+"class = " + "'resultI'" +"src="+ "'/images/i1.png'></img>";
+var resultImage2 = "<img "+"class = " + "'resultI'" +"src="+ "'/images/i2.png'></img>";
+var resultImage3 = "<img "+"class = " + "'resultI'" +"src="+ "'/images/i3.png'></img>";
+
+
+if(randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = resultImage1 + " Bhai mai jeeta";    
+}
+else if(randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML = resultImage2 + " Bhai tu jeeta";
+}
+else {
+    document.querySelector("h1").innerHTML = resultImage3 + " Bhai dubara phekhde";
+}
